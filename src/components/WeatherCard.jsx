7e81c2da8d2d72f6efacc696ac4a5706817bdb76
ThaiNgocLeDuy <64624794+ThaiNgocLeDuy@users.noreply.github.com>
2021-08-09 18:37:12 +0700

    Update WeatherCard.jsx

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 function WeatherCard(props) {
-    //dt: key trong 1 list api tra ve
-    //main: nhung yeu to chinh nhu nhiet do, do am,...
+  //dt: key trong 1 list api tra ve
+  //main: nhung yeu to chinh nhu nhiet do, do am,...
 
   const { dt, temp_min, temp_max, main, icon } = props;
   const date = new Date(dt);
@@ -16,7 +16,7 @@ function WeatherCard(props) {
       <div className="card-body">
         <h5 className="card-title">{main}</h5>
         <p className="card-text">
-          {date.toLocaleDateString('en-GB')} - {date.toLocaleTimeString()}
+          {date.toLocaleDateString("en-GB")} - {date.toLocaleTimeString()}
         </p>
         <p className="card-text">Min: {Math.trunc(temp_min - 273.15)} ℃</p>
         <p className="card-text">Max: {Math.trunc(temp_max - 273.15)} ℃</p>
